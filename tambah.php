29ae10a12467a7ceb7ac5808d24f581a7532a265
atid <asatidzal.hakiim3@smk.belajar.id>
2025-01-17 10:31:05 +0700

    Update tambah.php

---
+++ tambah.php
@@ -0,0 +1,23 @@
+<title>Tambah Siswa</title>
+<form action="" method="post">
+    <fieldset style="width:30%">
+        <legend>Tambah siswa</legend>
+        <p>Nama</p>
+        <input type="text" name:"nama" id:"nama" required>
+        <p>Kelas</p>
+        <select name="kelas" id="kelas" required>
+
+            <option value=""> -Pilih Kelas- </option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+        </select>
+        <P>jURUSAN</P>
+        <input type="radio" name="jurusan" id="jurusan" value="PPLG" required>PPLG
+        <input type="radio" name="jurusan" id="jurusan" value="AKL" required>AKL
+        <p>
+            <input type="reset" value="Reset">
+            <input type="submit" value="Simpan">
+        </p>
+    </fieldset>
+</form>
